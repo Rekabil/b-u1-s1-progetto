8e152f43815dcb96b4efe491c7d82039e70956a8
Bilgen Kaan Remzi <137759282+Rekabil@users.noreply.github.com>
2023-10-06 11:32:04 +0200

    Play

--- Main.java
+++ Main.java
@@ -1,3 +1,12 @@
+import src.Audio;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Audio song = new Audio("Name",2 , 6 );
+
+        song.play();
+    }
+
 
 }
--- src/Audio.java
+++ src/Audio.java
@@ -2,7 +2,18 @@ package src;
 
 public class Audio {
     private String titolo;
-    private String Volume;
+    private int volume;
     private int durata;
 
+    public Audio(String titolo, int volume, int durata) {
+        this.titolo = titolo;
+        this.volume = volume;
+        this.durata = durata;
+    }
+
+    public void play() {
+        for (int i = 0; i< durata ; i++) {
+        System.out.println("Titolo: " + titolo + " Durata: " + durata + " Volume: " + "!".repeat(volume) );
+        }
+    }
 }
